25cc554c3492f2b3e973c3f5ec21fbff79125d17
crd <957573373@qq.com>
2022-11-13 22:10:47 +0800

    加点-第四次提交

--- src/components/library/add-point.vue
+++ src/components/library/add-point.vue
@@ -5,8 +5,11 @@
                 <el-col :span="4">
                     <span>名称：</span>
                 </el-col>
-                <el-col :span="20">
+                <el-col :span="16">
                     <el-input v-model="labelText"  placeholder="请输入名称"  />
+                </el-col>
+                <el-col :span="4">
+                    <el-button size="small"  @click="dialogVisible = true">点样式</el-button>
                 </el-col>
             </el-row>
         </div>
@@ -37,7 +40,31 @@
         </div>
         <div class="point-option">
             <el-tabs type="border-card" stretch>
-                <el-tab-pane label="说明">1</el-tab-pane>
+                <el-tab-pane label="说明">
+                    <el-row >
+                      <el-col :span="12">
+                        <el-button size="small" @click="open">选择图片</el-button>
+                      </el-col>
+                      <el-col :span="12">
+                        <span>描述文字</span>
+                      </el-col>
+                    </el-row>
+                    <el-row >
+                      <el-col :span="12">
+                        <img v-if="objectURL" style="width: 200px; height: 180px;padding: 8px;" :src="objectURL" :fit="cover" />
+                        <el-image v-else style="width: 200px; height: 180px;padding: 8px;"  />
+                      </el-col>
+                      <el-col :span="12">
+                        <el-input
+                          v-model="textarea"
+                          :rows="7"
+                          type="textarea"
+                          placeholder="Please input"
+                          style="padding:5px"
+                        />
+                      </el-col>
+                    </el-row>
+                </el-tab-pane>
                 <el-tab-pane label="样式/颜色">
                     <div>
                         <p>标签设置</p>
@@ -101,7 +128,35 @@
                 </el-tab-pane>
             </el-tabs>
         </div>
+        <el-row justify="end" align="middle">
+            <el-col :span="8">
+                <el-button size="small" @click="saveEntityFn">保存</el-button>
+                <el-button size="small" @click="clearEntityFn">清除所有</el-button>
+            </el-col>
+        </el-row>
     </div>
+    <el-dialog
+        v-model="dialogVisible"
+        title="点样式"
+        width="39%"
+    >
+        <div class="point-image">
+            <el-image
+            style="width: 35px; height: 35px;margin: 2px 2px;padding: 2px 2px;"
+            v-for="item in pngIconFile"
+            :key="item"
+            :src="item"
+            @click="iconSrc=item"
+            />
+        </div>
+        <el-image style="width: 60px; height: 60px;"  :src="iconSrc" />
+        <template #footer>
+            <span class="dialog-footer">
+                <el-button @click="dialogVisible = false">取消</el-button>
+                <el-button type="primary" @click="dialogVisible = false">确定</el-button>
+            </span>
+        </template>
+    </el-dialog>
   </template>
 
 <script>
@@ -113,16 +168,28 @@ import { car3Tran, CGCSToLonLat } from '@/utils/singlecoortran.js'
 import { nanoid } from 'nanoid'
 // import { watchOnce } from '@vueuse/core'
 import * as Cesium from 'cesium'
+import { ElMessage } from 'element-plus'
 // import { layoutStore } from '@/store/layoutStore.js'
 // import { storeToRefs } from 'pinia'
 // import { useRoute } from 'vue-router'
+import { useFileDialog } from '@vueuse/core'
 
 export default defineComponent({
   name: 'addPoint',
   setup () {
-    const pointIdRandom = nanoid(12) // 设置随机id
-    const pointId = 'add-point-' + pointIdRandom
-    const entityBlankPoint = useBlankPoint(pointId)
+    // 说明 选择图片
+    const { files, open } = useFileDialog()
+    const objectURL = ref(null)
+    watch(files, () => {
+      objectURL.value = URL.createObjectURL(files.value[0])
+      console.log(objectURL.value)
+    })
+    // 描述文字
+    const textarea = ref(null)
+    // 初始化时 先设置好一个entity的参数
+    let pointIdRandom = nanoid(12) // 设置随机id
+    let pointId = 'add-point-' + pointIdRandom
+    let entityBlankPoint = useBlankPoint(pointId)
 
     // 鼠标左键点击事件
     const { coordinate: coorClick } = useMouseLeftClick('addPoint', () => {
@@ -164,13 +231,24 @@ export default defineComponent({
     ])
     // 点的高度
     const pointHeight = ref(0.00)
+    // 选择点样式
+    const dialogVisible = ref(false)
+    const pngIconFile = []
+    const importFn = import.meta.glob('/src/assets/icon/*.png')
+    for (const path in importFn) {
+      importFn[path]().then(mod => {
+        pngIconFile.push(mod.default)
+      })
+    }
+    const iconSrc = ref('/src/assets/icon/ylw-pushpin.png')
     // 需要修改的选项
-    const objOption = reactive({ labelText, labelScale, labelColor, pointScale, pointColor, pointHeight })
+    const objOption = reactive({ labelText, labelScale, labelColor, pointScale, pointColor, pointHeight, iconSrc })
     watch(objOption, () => {
       const entityById = window.viewer.entities.getById(pointId)
       entityById.label.text = objOption.labelText
       entityById.label.scale = objOption.labelScale
       entityById.label.fillColor = Cesium.Color.fromCssColorString(objOption.labelColor)
+      entityById.billboard.image = objOption.iconSrc
       entityById.billboard.scale = objOption.pointScale
       entityById.billboard.color = Cesium.Color.fromCssColorString(objOption.pointColor)
       if (objOption.pointHeight !== 0.0) {
@@ -233,6 +311,69 @@ export default defineComponent({
       }
     }, { deep: true })
 
+    // 保存功能
+    const allEntity = []
+    const allEntityId = []
+    const saveEntityFn = () => {
+      if (!coorClick.value) {
+        ElMessage({
+          message: '清先添加数据!',
+          type: 'warning'
+        })
+      }
+      // 初始化
+      if (coorClick.value) {
+        allEntity.push(entityBlankPoint)
+        allEntityId.push(pointId)
+        pointIdRandom = nanoid(12) // 设置随机id
+        pointId = 'add-point-' + pointIdRandom
+        entityBlankPoint = useBlankPoint(pointId)
+        window.viewer.entities.add(entityBlankPoint)
+        // 初始化坐标
+        coorClick.value = null
+        // 初始化名称
+        labelText.value = '未命名点'
+        // 初始化坐标系类型
+        coorType.value = 'CGCS'
+        // 初始化标签的比例颜色
+        labelScale.value = 1
+        labelColor.value = 'rgba(255, 255, 255, 1)'
+        // 初始化点的比例颜色
+        pointScale.value = 0.5
+        pointColor.value = 'rgba(255, 255, 255, 1)'
+        // 初始化点的高度
+        pointHeight.value = 0
+        // 初始化样式
+        iconSrc.value = '/src/assets/icon/ylw-pushpin.png'
+        ElMessage({
+          message: '保存成功',
+          type: 'success'
+        })
+      }
+      coorClick.value = null
+    }
+    // 清除功能
+    const clearEntityFn = () => {
+      if (allEntity.length === 0) {
+        ElMessage({
+          message: '清先添加数据!',
+          type: 'warning'
+        })
+      }
+      if (allEntity.length !== 0) {
+        allEntity.forEach(item => {
+          window.viewer.entities.remove(item)
+        })
+        allEntity.splice(0, allEntity.length)
+        allEntityId.splice(0, allEntity.length)
+        coorClick.value = null
+        ElMessage({
+          message: '清除数据成功!',
+          type: 'success'
+        })
+      }
+    }
+
     return {
       labelColor,
       labelPredefineColors,
@@ -247,7 +388,16 @@ export default defineComponent({
       coorBidirectional,
       LL,
       CGCS,
-      LC
+      LC,
+      saveEntityFn,
+      clearEntityFn,
+      dialogVisible,
+      pngIconFile,
+      iconSrc,
+      files,
+      open,
+      objectURL,
+      textarea
     }
   }
 })
@@ -290,6 +440,7 @@ const coorToFour = (coorClick, coorType) => {
         }
     }
     .point-option{
+        margin: 10px 0;
         .el-tabs{
             p{
                 margin: 10px 0;
@@ -307,4 +458,17 @@ const coorToFour = (coorClick, coorType) => {
         }
     }
 }
+.el-dialog{
+    .point-image{
+        .el-image{
+            &:hover {
+                background-color: rgba(128,128,128, 0.3);
+            }
+        }
+    }
+    .dialog-footer button:first-child {
+        margin-right: 10px;
+    }
+}
+
 </style>
